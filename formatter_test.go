e273d990522026e623ab31f7ffbe9f3c2cf614c6
RyoGreen <41415120ryo@gmail.com>
2023-05-21 00:05:37 +0900

    add conma test

--- formatter_test.go
+++ formatter_test.go
@@ -14,7 +14,18 @@ func TestFormatInt(t *testing.T) {
 		expectedResult string
 		expectedError  error
 	}{
+		{1, true, true, "¥1-", nil},
+		{10, true, true, "¥10-", nil},
 		{100, true, true, "¥100-", nil},
+		{1000, true, true, "¥1,000-", nil},
+		{10000, true, true, "¥10,000-", nil},
+		{100000, true, true, "¥100,000-", nil},
+		{1000000, true, true, "¥1,000,000-", nil},
+		{10000000, true, true, "¥10,000,000-", nil},
+		{100000000, true, true, "¥100,000,000-", nil},
+		{1000000000, true, true, "¥1,000,000,000-", nil},
+		{10000000000, true, true, "¥10,000,000,000-", nil},
+		{100000000000, true, true, "¥100,000,000,000-", nil},
 		{1000, true, false, "¥1,000", nil},
 		{1000000, false, true, "1,000,000-", nil},
 		{1000000, false, false, "1,000,000", nil},
