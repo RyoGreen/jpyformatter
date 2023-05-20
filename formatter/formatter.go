5dac863ffed796a3a0f97e4d8aad2c9d37deae1b
RyoGreen <41415120ryo@gmail.com>
2023-05-20 23:08:53 +0900

    make format function

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -1,6 +1,9 @@
 package formatter
 
 import (
+	"errors"
+	"strconv"
+
 	"golang.org/x/exp/constraints"
 )
 
@@ -8,7 +11,26 @@ type unit interface {
 	constraints.Signed | constraints.Float
 }
 
-func Format[U unit](price U, prefixEnabled, suffixEnabled bool) string {
-	var result string
-	return result
+func Format[U unit](price U, prefixEnabled, suffixEnabled bool) (string, error) {
+	p := int(price)
+	if p >= 1000000000000 {
+		return "", errors.New("error: price exceeds 1000000000000 yen")
+	}
+	str := strconv.Itoa(p)
+	var result []byte
+	count := 0
+	for i := len(str) - 1; i >= 0; i-- {
+		if count%3 == 0 && len(str) != count && count != 0 {
+			result = append([]byte{','}, result...)
+		}
+		result = append([]byte{str[i]}, result...)
+		count++
+	}
+	if prefixEnabled {
+		result = append([]byte{0xC2, 0xA5}, result...)
+	}
+	if suffixEnabled {
+		result = append(result, '-')
+	}
+	return string(result), nil
 }
